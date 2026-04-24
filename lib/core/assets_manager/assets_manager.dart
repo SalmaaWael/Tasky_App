9fa8a5d0ca6e56b4003b1ed91c12fc67ae8241a6
Salma Wael <swaelghoneim@gmail.com>
2026-04-24 18:49:41 +0300

    add splash_screen native

---
+++ lib/core/assets_manager/assets_manager.dart
@@ -0,0 +1,10 @@
+abstract class AssetsManager{
+  static const String base ="assets/images";
+  static const String appIcon ="$base/app_icon.png";
+  static const String homeImage="$base/home_image.png";
+  static const String logo="$base/logo.png";
+  static const String onboard1="$base/onboard1.png";
+  static const String onboard2="$base/onboard2.png";
+  static const String onboard3="$base/onboard3.png";
+
+}
---
+++ lib/core/colors_manager/colors_manager.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+abstract class ColorsManager{
+  static const Color primaryColor =Color(0xff5F33E1);
+  static const Color textLargeColor =Color(0xff24252C);
+  static const Color textSmallColor =Color(0xff6E6A7C);
+}
