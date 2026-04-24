b6896ffd1333127141b584e8528718c6d75fa04a
Salma Wael <swaelghoneim@gmail.com>
2026-04-24 15:27:41 +0300

    create project flutter

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TaskyApp());
+}
+
+class TaskyApp extends StatelessWidget {
+  const TaskyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
